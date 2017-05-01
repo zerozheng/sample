17e2852c1e9d27db84b779b5182b74fc7ed38287
zerozheng <641486122@qq.com>
2017-05-01 13:06:23 +0000

    finish user signup

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.default')
 @section('title','注册')
 @section('content')
-<h1>注册</h1>
+<div class="col-md-offset-2 col-md-8">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h5>注册</h5>
+    </div>
+    <div class="panel-body">
+      @include('shared.errors')
+
+      <form action="{{ route('users.store')}}" method="post">
+        {{ csrf_field()  }}
+        <div class="form-group">
+          <label for="name">名称：</label>
+          <input type="text" name="name" class='form-control' value="{{ old('name') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password">邮箱：</label>
+          <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+        </div>
+
+        <div class="form-group">
+          <label for="password">密码：</label>
+          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+        </div>
+
+        <button type="submit" class="btn btn-primary">注册</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
